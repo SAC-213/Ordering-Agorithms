7cee13ac03a85542c7af7d2029668f81630a1203
SAC-213 <ascencioncruzsaul@gmail.com>
2025-10-17 18:07:08 -0600

    main

---
+++ Binaria.c
@@ -0,0 +1,121 @@
+/*                                  *
+*   Alumno: Saul Ascencion Cruz     *
+*   Grupo: 2BM2                     *
+*                                   *
+*                                   *
+*   -   -   -   -   -   -   -   -   *
+*/   
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+
+//la mitad de 10,000,000 se encuentra en 1073782848
+//la mitad de 5,000,000 se encuentra en 1073807161
+
+void QuickSort(int A[], int p, int r);
+int Pivot(int A[], int p, int r);
+void Intercambio(int A[], int i, int j);
+int Binaria(int A[], int valor, int inicio, int final, int *Comparaciones);
+
+int main(int argc, char const *argv[])
+{
+    int n = atoi(argv[1]);
+    int valor = atoi(argv[2]);
+    int *A;
+    int Comparaciones=0;
+    int res;
+
+    //Apartar memoria para n números enteros
+    A=malloc(n*sizeof(int));
+    if(A==NULL)
+    {
+        printf("\nError al intentar reservar memoria para %d elementos\n",n);      
+        exit(1);
+    }   
+
+
+    for(int i=0;i<n;i++)
+        scanf("%d",&A[i]);
+
+    //ordenar
+    QuickSort(A,0,n-1);
+    
+    res=Binaria(A,valor,0,n-1,&Comparaciones);
+    printf("\nEl valor ");
+    (res==TRUE) ? printf("SI ") : printf("NO ");
+    printf("fue encontrado.\nEn %d comparaciones\n",Comparaciones);
+
+
+    free(A);
+    return 0;
+}
+
+void QuickSort(int A[], int p, int r){
+    int j;
+    if (p<r)
+    {
+        j = Pivot(A,p,r);
+        QuickSort(A, p, j-1);
+        QuickSort(A, j+1, r);
+    }
+}
+
+int Pivot(int A[], int p, int r){
+    int piv=A[p];
+    int i=p+1;
+    int j=r;
+
+    while (i<j)
+    {
+        while (A[i]<=piv && i<=r){
+            i++;
+        }
+        
+        while (A[j]>piv && j>=p){
+            j--;
+        }
+
+        if (i<j)
+        {
+            Intercambio(A,i,j);
+        }  
+    }
+    Intercambio(A,p,j);
+    return j;
+}
+
+void Intercambio(int A[], int i, int j){
+    int temp=A[j];
+    A[j]=A[i];
+    A[i]=temp;
+}
+
+int Binaria(int A[], int valor, int inicio, int final, int *Comparaciones){
+    int res;
+    int mitad;
+    res=FALSE;
+    while (inicio<=final)
+    {
+        mitad = (inicio+final)/2;
+
+        (*Comparaciones)++;
+        if (valor==A[mitad])
+        {
+            res = TRUE;
+            break;
+        }
+
+        if (valor>A[mitad])
+        {
+            inicio = mitad+1;
+        }
+        else
+        {
+            final = mitad-1;
+        }
+    }
+    return res;
+}
---
+++ Secuencial.c
@@ -0,0 +1,53 @@
+/*                                  *
+*   Alumno: Saul Ascencion Cruz     *
+*   Grupo: 2BM2                     *
+*                                   *
+*                                   *
+*   -   -   -   -   -   -   -   -   *
+*/   
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+// el primer numero desordenado de 10,000,000 está en 856834115 y el último en 2045206161
+// el primer numero desordenado de 5,000,000 está en 856834115 y el último en 876774664
+
+void lineal(int A[], int *res, int *Comparaciones, int *n, int *valor);
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    int Comparaciones = 0;
+    int res = FALSE;
+    int n = atoi(argv[1]);
+    int valor = atoi(argv[2]);
+    int *A=malloc(sizeof(int) * n);
+
+    for ( i = 0; i < n; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+
+    lineal(A,&res,&Comparaciones, &n, &valor);
+
+    printf("\nEl valor ");
+    (res==TRUE) ? printf("SI ") : printf("NO ");
+    printf("fue encontrado.\nEn %d comparaciones\n",Comparaciones);
+
+    return 0;
+}
+
+void lineal(int A[], int *res, int *Comparaciones, int *n, int *valor)
+{
+    for (int  i = 0; i < (*n); i++)
+    {
+        (*Comparaciones)++;
+        if (A[i]==(*valor))
+        {
+            (*res) = TRUE;
+            break;
+        }
+    }
+    res = FALSE;
+}
